d42cbce5b9822ce4b4f23cfd9acccbc5c0356d15
RobWiederstein <khuon68@gmail.com>
2015-06-11 21:05:09 -0500

    built SAAR functions

--- R/functions_SAAR.R
+++ R/functions_SAAR.R
@@ -1,3 +1,6 @@
+
+
+# import saar datasets by year----
 import_SAAR1999 <- function(){
     a <- read.csv(paste(getwd(), "/data_raw/SAAR1999.csv", sep = ""), 
              skip = 6, 
@@ -202,6 +205,8 @@ import_SAAR2013 <- function(){
              )
     )
 }
+
+#clean each year's data for merge ----
 #index column for merge using 1999 as base year
 create_index <- function(){
     district.index <- import_SAAR1999()[,1]        #school districts
@@ -396,7 +401,7 @@ spruce_SAAR2013 <- function(temp){
     temp
 }
 
-#go from wide to long
+#go from wide to long format ----
 wide_2_long_1999 <- function(temp){
     library(reshape)
     temp$year <- rep(1999, nrow(temp))
@@ -487,3 +492,52 @@ wide_2_long_2013 <- function(temp){
     names(temp) <- tolower(names(temp))
     temp <- melt(temp, id.vars = c("year", "district"), variable_name = "grade")
 }
+
+#merge the datasets ----
+merge_SAAR_ALL <- function(){
+    #get functions
+    #source("./R/functions_SAAR.R")
+    
+    #import annual data sets, spruce them up, change format from wide to long
+    
+    SAAR1999 <- wide_2_long_1999(spruce_SAAR1999(import_SAAR1999()))
+    SAAR2000 <- wide_2_long_2000(spruce_SAAR2000(import_SAAR2000()))
+    SAAR2001 <- wide_2_long_2001(spruce_SAAR2001(import_SAAR2001()))
+    SAAR2002 <- wide_2_long_2002(spruce_SAAR2002(import_SAAR2002()))
+    SAAR2003 <- wide_2_long_2003(spruce_SAAR2003(import_SAAR2003()))
+    SAAR2004 <- wide_2_long_2004(spruce_SAAR2004(import_SAAR2004()))
+    SAAR2005 <- wide_2_long_2005(spruce_SAAR2005(import_SAAR2005()))
+    SAAR2006 <- wide_2_long_2006(spruce_SAAR2006(import_SAAR2006()))
+    SAAR2007 <- wide_2_long_2007(spruce_SAAR2007(import_SAAR2007()))
+    SAAR2008 <- wide_2_long_2008(spruce_SAAR2008(import_SAAR2008()))
+    SAAR2009 <- wide_2_long_2009(spruce_SAAR2009(import_SAAR2009()))
+    SAAR2010 <- wide_2_long_2010(spruce_SAAR2010(import_SAAR2010()))
+    SAAR2011 <- wide_2_long_2011(spruce_SAAR2011(import_SAAR2011()))
+    SAAR2012 <- wide_2_long_2012(spruce_SAAR2012(import_SAAR2012()))
+    SAAR2013 <- wide_2_long_2013(spruce_SAAR2013(import_SAAR2013()))
+    
+    
+    #combine dataframes
+    df <- data.frame(
+        rbind(
+            SAAR1999, SAAR2000, SAAR2001, SAAR2002, SAAR2003, SAAR2004, 
+            SAAR2005, SAAR2006, SAAR2007, SAAR2008, SAAR2009, SAAR2010, 
+            SAAR2011, SAAR2012, SAAR2013
+        ), 
+        stringsAsFactors = F
+    )
+    df
+}
+
+#write it out into R object ----
+write_SAAR_ALL <- function(){
+    df <- merge_SAAR_ALL()
+    #write out r object to "objects" directory
+    wd <- getwd()
+    dir <- "objects"
+    file <- "saar_1999_2013"
+    myfile <- paste(wd, dir, file, sep = "/")
+    save(df, file = myfile)
+}
+
+
